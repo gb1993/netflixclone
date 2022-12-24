c438d7b7bd0700add10656d054c3f89931eef604
Gabriel Brancom <gbanco1993@gmail.com>
2022-12-23 23:32:19 -0300

    fix: prepara app para deploy

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import NotFound from './pages/NotFound';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route index path='/' element={<Users />} />
       <Route exact path='/app' element={<App />} />
