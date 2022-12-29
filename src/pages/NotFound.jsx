b16823f9453f2047bcb2efe1d12e61878257aaf7
Gabriel Brancom <gbanco1993@gmail.com>
2022-12-28 21:42:35 -0300

    adiciona estilo na pagina notfound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <>
-            <h1>404</h1>
+        <div style={{width: '100%', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
+            <h1 style={{color: 'red', fontWeight: 700, fontSize: '50px'}}>404</h1>
             <p>Página Não encontrada</p>
-        </>
+            <Link to="/app">
+                <button>Clique para Voltar</button>
+            </Link>
+        </div>
     )
 }
 
