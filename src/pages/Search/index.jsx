73c39cf681e698ac65e2893f127ab641b734c7b2
Gabriel Brancom <gbanco1993@gmail.com>
2023-01-05 23:51:57 -0300

    fix: data de estreia na pagina de busca

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -8,7 +8,7 @@ const Search = () => {
     const location = useLocation();
     const {state: {getSearchList}} = location;
     const navigate = useNavigate();
-
+    console.log(getSearchList)
     const handleTrailer = async (id) => {
         const youtubeTrailerId = await getTraillerId(id);
         if (youtubeTrailerId === false) {
@@ -17,36 +17,34 @@ const Search = () => {
             const youtubeLink = `https://www.youtube.com/embed/${youtubeTrailerId}`;
             navigate("/trailer", {state: {link: youtubeLink}});
         }
-      }
+    }
 
     return (
         <div className="search">
-            {!getSearchList ? <div className="loading" /> : 
-                <>
+            <>
                 <Link to="/app"><img src={goBackIcon} alt="go back icon" /></Link>
-                    <div className="search-container">
-                        {getSearchList.map((movie) => (
-                            movie.poster_path ?
-                                <button className="poster-container" key={movie.id} onClick={ () => handleTrailer(movie.id) }>
-                                    <div className="search-poster-detail">
-                                        <h3>{movie.title || movie.original_name}</h3>
-                                        <div>
-                                            <span style={{color: movie.vote_average > 5 ? 'green' : 'red'}}>{movie.vote_average.toFixed(1)} pontos</span>
-                                            <span>|</span>
-                                            <span>{(movie.release_date || movie.first_air_date).substr(0, 4)}</span>
-                                        </div>
-                                        <p>{movie.overview.substr(0, 50)}...</p>
+                <div className="search-container">
+                    {getSearchList && getSearchList.map((movie) => (
+                        movie.poster_path ?
+                            <button className="poster-container" key={movie.id} onClick={ () => handleTrailer(movie.id) }>
+                                <div className="search-poster-detail">
+                                    <h3>{movie.title || movie.original_name}</h3>
+                                    <div>
+                                        <span style={{color: movie.vote_average > 5 ? 'green' : 'red'}}>{movie.vote_average.toFixed(1)} pontos</span>
+                                        <span>|</span>
+                                        <span>{movie.release_date?.substr(0, 4) || 'N/A'}</span>
                                     </div>
-                                    <img
-                                        src={`https://tmdb.org/t/p/w300${movie.poster_path}`}
-                                        alt={movie.original_name}
-                                    />
-                                </button>
-                            : ''
-                        ))}
-                    </div>
-                </>
-            }
+                                    <p>{movie.overview.substr(0, 50) || 'N/A' }...</p>
+                                </div>
+                                <img
+                                    src={`https://tmdb.org/t/p/w300${movie.poster_path}`}
+                                    alt={movie.original_name}
+                                />
+                            </button>
+                        : ''
+                    ))}
+                </div>
+            </>
         </div>
     )
 }
